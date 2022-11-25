484122fd765647e539e29e75f3533d364217804c
Xayah <zds1249475336@gmail.com>
2022-11-26 00:00:12 +0800

    feat: 初始化工程
    Change-Id: I96390b6c16e15877aca23bd2edaf61e4e82856cf

---
+++ app/src/main/java/com/xayah/zstandardforandroid/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.xayah.zstandardforandroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import com.xayah.zstandardforandroid.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+}
